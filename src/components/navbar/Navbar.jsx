f16edb4582c4d95b57992b3130c9a67137971184
Ankur <iamankursin@gmail.com>
2023-11-30 14:46:08 +0530

    updates in navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = ()=>{
             <div className="wrapper">
                 <motion.span initial= {{opacity:0,scale:0.5} }animate= {{opacity:1, scale:1}} transition={{duration : 0.5}}></motion.span>
                 <div className="social">
-                    <a href="https://instagram.com/the.ankursingh_?igshid=OGQ5ZDc2ODk2ZA=="><img src="/instagram.png" alt="" /></a>
+                    <a href="https://instaclone-wine.vercel.app/"><img src="/instagram.png" alt="" /></a>
                     <a href="https://github.com/AnkurSingh1411"><img src="/github.png" alt="" /></a>
                     <a href="https://www.youtube.com/channel/UCR-fnDs33F00DD3leK-i6Fw"><img src="/youtube.png" alt="" /></a>
                     <a href="https://www.linkedin.com/in/ankur-singh-0a63571a1/"><img src="/linkedin.png" alt="" /></a>
